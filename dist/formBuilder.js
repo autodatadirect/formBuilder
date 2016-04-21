e1706c391605b6ff9374f741f75021da1d7c23a3
Martin Church <mchurch@add123.com>
2016-04-21 10:11:15 -0400

    compile for 2.1.5

--- dist/formBuilder.js
+++ dist/formBuilder.js
@@ -1,6 +1,6 @@
 /** 
  * formBuilder - An advanced HTML5 form creation & validation framework
- * @version v2.1.4
+ * @version v2.1.5
  * @link http://autodatadirect.github.io/formBuilder/
  * @repository https://github.com/autodatadirect/formBuilder
  * @license MIT
@@ -5681,9 +5681,7 @@
 			self.filterValue = '';
 			self.shim.text('');
 
-			if(!andSelectNothing) {
-				self._handleItemNotFound();
-			}
+			self._handleItemNotFound();
 
 			self.inputWidget.redraw();
 		},
--- dist/locales/formBuilder_french.fr.js
+++ dist/locales/formBuilder_french.fr.js
@@ -1,6 +1,6 @@
 /** 
  * formBuilder - An advanced HTML5 form creation & validation framework
- * @version v2.1.4
+ * @version v2.1.5
  * @link http://autodatadirect.github.io/formBuilder/
  * @repository https://github.com/autodatadirect/formBuilder
  * @license MIT
