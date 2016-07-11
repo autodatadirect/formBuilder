b2841f83c8f2030dbfe22c851bfcd45f90b3f7c2
Paul Anderson <panderson@add123.com>
2016-07-11 16:38:01 -0400

    rebuilding 2.3.3 dist

--- dist/locales/add-formbuilder_french.fr.js
+++ dist/locales/add-formbuilder_french.fr.js
@@ -1,6 +1,6 @@
 /** 
  * add-formbuilder - An advanced HTML5 form creation and validation framework
- * @version v2.3.1
+ * @version v2.3.3
  * @link http://autodatadirect.github.io/formBuilder/
  * @repository https://github.com/autodatadirect/formBuilder
  * @license MIT
