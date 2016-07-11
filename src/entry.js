20b69fd96666c2315a763472a6b4a358713574d9
Paul Anderson <panderson@add123.com>
2016-07-11 17:00:40 -0400

    trying to not use nested js in entry

--- src/entry.js
+++ src/entry.js
@@ -1,5 +1,12 @@
 'use strict';
 
-require('../dist/add-formbuilder.nested.min.js');
+require('expose?jQuery!jquery');
+require('jquery-ui');
+require('normalize.css');
+require('moment');
+require('bootstrap-datepicker');
+require('spin.js');
+require('timepicker');
+require('../dist/add-formbuilder.js');
 require('../dist/css/formBuilder.css');
 require('../dist/css/add-formbuilder.nested.css');
