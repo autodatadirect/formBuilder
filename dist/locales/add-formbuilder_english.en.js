cacd174ddc86822188913fcaafbe675f6256d54c
Sergio Rodriguez <srodriguez@add123.com>
2016-08-25 09:12:00 -0400

    Build version 2.3.6

--- dist/locales/add-formbuilder_english.en.js
+++ dist/locales/add-formbuilder_english.en.js
@@ -1,6 +1,6 @@
 /** 
  * add-formbuilder - An advanced HTML5 form creation and validation framework
- * @version v2.3.5
+ * @version v2.3.6
  * @link http://autodatadirect.github.io/formBuilder/
  * @repository https://github.com/autodatadirect/formBuilder
  * @license MIT
