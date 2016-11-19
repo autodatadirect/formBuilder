f811acce6c80e2954f4da9c291b43c7fe2157e4f
Charles Howard <cdhoward@add123.com>
2016-11-19 17:50:58 -0500

    ported the phone type to webpack

--- src/types/index.js
+++ src/types/index.js
@@ -1,6 +1,7 @@
 import text from './text';
 import select from './select';
+import phone from './phone';
 
-const types = {text, select};
+const types = {text, select, phone};
 
 export default types;
