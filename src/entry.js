0e37f8f2898bebc1f25498d090a01bbc56a4ced4
Charles Howard <cdhoward@add123.com>
2016-11-21 10:21:17 -0500

    ported dropDownPanel

--- src/entry.js
+++ src/entry.js
@@ -3,6 +3,7 @@ import './widgets/formBuilder';
 import './widgets/arrayField';
 import './widgets/dateRangePicker';
 import './widgets/dateTimeRangePicker';
+import './widgets/dropDownPanel';
 import {register} from './types/index';
 import allTypes from './types/allTypes';
 register(allTypes);
