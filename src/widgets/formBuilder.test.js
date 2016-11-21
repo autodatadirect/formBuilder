950d33581bc699d273392704d2f04eb836579a28
Charles Howard <cdhoward@add123.com>
2016-11-21 10:22:58 -0500

    removed white space

--- src/widgets/formBuilder.test.js
+++ src/widgets/formBuilder.test.js
@@ -3,15 +3,10 @@ import expect from 'expect';
 import 'jquery-ui/ui/widget';
 import './formBuilder';
 
-
 describe('formBuilder Widget', function(){
 	
 	it('to work', () => {
 		expect(true).toExist();
 	});
-
-	
-
-	
 
 });
