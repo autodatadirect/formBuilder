9bba90a33c20b1aa0603904c2d30b94748afe21b
Charles Howard <cdhoward@add123.com>
2016-11-21 23:07:29 -0500

    Experimenting breaking widgets into two different files so that they can be unit tested outside of the jQuery widget infrastructure

--- src/widgets/fieldWidget.test.js
+++ src/widgets/fieldWidget.test.js
@@ -1,28 +1,39 @@
-import  '../util/jsdomSetup';
+import def from './fieldWidget.def';
 import expect from 'expect';
-//import './fieldWidget';
-//import $ from 'jquery';
-//window.jQuery = $;
-import func from './fieldWidget';
+import $ from 'jquery';
 
-describe('fieldWidget Widget', function(){
+$.widget = () => {};
 
+describe('The fieldWidget', () => {
+	it('has all required methods', () => {
+		expect(def.isDirty).toExist();
+		expect(def.validate).toExist();
+		expect(def.clearDirty).toExist();
+		expect(def.clear).toExist();
+		expect(def.flash).toExist();
+		expect(def.set).toExist();
+		expect(def.get).toExist();
 
-
-	//console.log('********** jQuery', window.jQuery);
-
-	it('to have jquery', () => {
-		expect(window.jQuery).toExist();
-		expect(window.$).toExist();
+		expect(typeof def.isDirty).toBe('function');
+		expect(typeof def.validate).toBe('function');
+		expect(typeof def.clearDirty).toBe('function');
+		expect(typeof def.clear).toBe('function');
+		expect(typeof def.flash).toBe('function');
+		expect(typeof def.set).toBe('function');
+		expect(typeof def.get).toBe('function');
 	});
 
-	it('to have jquery UI', () => {
-		expect(window.$.widget).toExist();
+	it('returns NULL when get is called', () => {
+		expect(def.get()).toBe(null);
 	});
 
-	it('to exist', () => {
-		func();
+	it('returns calls set with undefined when clear is called', () => {
+		const spy = expect.spyOn(def, 'set');
+		expect(spy.calls.length).toBe(0);
+		def.clear();
+		def.clear();
+		expect(spy.calls.length).toBe(2);
+		expect(spy.calls[0].arguments.length).toBe(0);
+		spy.restore();
 	});
-
-
 });
