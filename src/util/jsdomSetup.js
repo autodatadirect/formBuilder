d8001317e97baf1c65510e76823420c597724247
Charles Howard <cdhoward@add123.com>
2017-03-23 15:31:38 -0400

    adding VERSION to testing environment

--- src/util/jsdomSetup.js
+++ src/util/jsdomSetup.js
@@ -3,4 +3,5 @@ if(!global.document){
 	global.document = jsdom.jsdom('<!doctype html><head></head><html><body></body></html>');
 	global.window = document.defaultView;
 	global.self = global.window;
+	global.VERSION = 'testing';
 }
